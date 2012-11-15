084fb67ba8d43990e239d06981227343a42c21c0
Johannes Steudle <johannes@steudle.eu>
2012-11-15 17:08:25 +0100

    path is initialized correctly now

--- oh-my-custom/paths.zsh
+++ oh-my-custom/paths.zsh
@@ -1,6 +1,7 @@
 #
 # global paths
 #
-export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
+# export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
+export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
 export PATH=$PATH:/Users/johannes/android-sdks/tools
 
