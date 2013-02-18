ab4e43d521e5dc1547419c4fcc5a1132e844a072
Johannes Steudle <johannes@steudle.eu>
2013-02-18 22:16:37 +0100

    changes to path variable

--- oh-my-custom/paths.zsh
+++ oh-my-custom/paths.zsh
@@ -4,5 +4,7 @@
 # export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
 export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
 export PATH=$PATH:/usr/local/texlive/2012/bin/x86_64-darwin
+# export PATH=$PATH:/System/Library/Frameworks/Python.framework/Versions/Current/bin
+export PATH=$PATH:/Volumes/jaybook2/Applications/Qt5.0.1/5.0.1/clang_64/bin
 export PATH=$PATH:/Users/johannes/android-sdks/tools
 
