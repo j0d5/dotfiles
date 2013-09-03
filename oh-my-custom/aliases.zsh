e39e524468b3f7b727540831c18a18dc10a68bb7
Johannes Steudle <johannes@steudle.eu>
2013-09-03 13:09:40 +0200

    updates

--- oh-my-custom/aliases.zsh
+++ oh-my-custom/aliases.zsh
@@ -7,8 +7,8 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias top='top -o cpu -n 30'
-alias mylogs='sudo tail -f /var/log/{system.log,secure.log}' 
-alias myip="wget -qO - http://checkip.dyndns.org | sed 's/[a-zA-Z<>/ :]//g'" 
+alias mylogs='sudo tail -f /var/log/{system.log,secure.log}'
+alias myip="wget -qO - http://checkip.dyndns.org | sed 's/[a-zA-Z<>/ :]//g'"
 ## command aliases
 alias x='exit'
 alias c='clear'
@@ -21,5 +21,3 @@ alias e='smartextract'
 alias o='open'
 alias fif='find . -type f \( -name "*.c*" -o -name "*.h" \) -print | xargs grep -in'
 alias loc='find . -type f \( -name "*.c*" -o -name "*.h" -o -name "*.py" -o -name "*.sh" \) -print | xargs cat | wc -l'
-alias gsak='wine .wine/drive_c/Program\ Files/gsak/gsak.exe'
-
