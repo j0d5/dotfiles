96b565521bd44d2ba98059095c42b39bcc566720
Johannes Steudle <johannes@steudle.eu>
2013-10-26 12:23:12 +0200

    added android sdk path

--- oh-my-custom/hosts/jaybook.local.path.zsh
+++ oh-my-custom/hosts/jaybook.local.path.zsh
@@ -1,7 +1,7 @@
 echo 'loading jaybook.path.zsh'
 export PATH=$HOME/.bin:$PATH
-export PATH=$PATH:/opt/local/bin:/opt/local/sbin
-export PATH=$PATH:/usr/texbin
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/usr/texbin
+export PATH=$PATH:/Volumes/jaybook2/Applications/android-sdk-macosx/tools:/Volumes/jaybook2/Applications/android-sdk-macosx/build-tools:/Volumes/jaybook2/Applications/android-sdk-macosx/platform-tools
 export PATH=$PATH:$HOME/QtSDK/Desktop/Qt/474/gcc/bin
 export NODE_PATH=/usr/local/lib/node
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages/:$PYTHONPATH
