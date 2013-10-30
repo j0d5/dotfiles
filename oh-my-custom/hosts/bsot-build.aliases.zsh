f136d6b8d5184d4d702e54ba5d89a996376b9d89
Johannes Steudle <jst2hi@bsot-build.(none)>
2013-10-30 09:31:10 +0100

    added alias and path file

---
+++ oh-my-custom/hosts/bsot-build.aliases.zsh
@@ -0,0 +1,20 @@
+# ant aliases
+alias antbd='ant clean debug -Dsdk.dir=/opt/android/android-sdk-linux -Dndk.dir=/opt/android/android-ndk-r9'
+alias antbr='ant clean release -Dsdk.dir=/opt/android/android-sdk-linux -Dndk.dir=/opt/android/android-ndk-r9'
+
+# android aliases
+alias andup='android update project -p .'
+alias andut='android update test-project -m ../ -p .'
+
+# adb aliases
+alias adbr='adb kill-server && adb start-server'
+alias adbk='adb -e emu kill'
+
+# start standard emulator or the emulator given in first argument
+adbks() {
+	if [[ -z $1 ]]; then
+		adb -e emu kill && emulator -avd google_emu_18 -no-window &
+	else
+		adb -e emu kill && emulator -avd $1 -no-window &
+	fi
+}
---
+++ oh-my-custom/hosts/bsot-build.path.zsh
@@ -0,0 +1,4 @@
+# local path file
+echo 'loading bsot-build.local.path.zsh'
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin
+export PATH=$PATH:/opt/android/android-sdk-linux/tools:/opt/android/android-sdk-linux/platform-tools:/opt/android/android-ndk-r9:/opt/android/android-sdk-linux/build-tools/android-4.3
