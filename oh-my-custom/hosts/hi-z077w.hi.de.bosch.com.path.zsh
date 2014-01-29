1972800c96ad6189469729ec5881276b13a36bb1
Johannes Steudle <external.johannes.steudle@bosch-softtec.com>
2014-01-29 17:18:19 +0100

    updates sublime work config, updates changed paths, updates gitconfig

--- oh-my-custom/hosts/hi-z077w.hi.de.bosch.com.path.zsh
+++ oh-my-custom/hosts/hi-z077w.hi.de.bosch.com.path.zsh
@@ -1,4 +1,4 @@
 # local path file
 echo 'loading tms-mac002.hi.de.bosch.com.path.zsh'
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin
-export PATH=$PATH:/Applications/android-sdk/tools:/Applications/android-sdk/platform-tools:/Applications/android-ndk-r9:/Applications/android-sdk/build-tools/android-4.3
+export PATH=$PATH:/usr/local/Cellar/android-sdk/22.3/tools:/usr/local/Cellar/android-sdk/22.3/platform-tools:/usr/local/Cellar/android-ndk/r9c:/usr/local/Cellar/android-sdk/22.3/build-tools/19.0.1
