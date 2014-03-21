1a8b3bc7f677e1b88c60b4d69873617e357b6587
Johannes Steudle <external.johannes.steudle@bosch-softtec.com>
2014-03-21 10:06:48 +0100

    Added files for build servers

---
+++ bsot-build.zsh
@@ -0,0 +1,4 @@
+echo 'bsot-build.zsh'
+# custom zsh commands
+export http_proxy='http://localhost:8080'
+export https_proxy='https://localhost:8080'
---
+++ macproserver.local.zsh
@@ -0,0 +1,4 @@
+echo 'loading macproserver.local.zsh'
+# custom zsh commands
+export http_proxy='http://localhost:8080'
+export https_proxy='https://localhost:8080'
