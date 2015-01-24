012626ab99e58720a50ef23bc5146c2f2b672d4d
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2015-01-24 15:35:39 +0100

    Add alias for opening path finder from console

--- oh-my-custom/hosts/Darwin.zsh
+++ oh-my-custom/hosts/Darwin.zsh
@@ -3,5 +3,6 @@ echo "i'm on a darwin system!"
 # general
 alias top='top -o cpu -n 30'
 alias downtime='pmset -g log | grep -e " Sleep  " -e " Wake  "'
+alias pf="open -a \"Path Finder.app\""
 
 plugins+=(osx brew)
