ee6a448c68330f6469cf951fe00767d4279f832a
Johannes Steudle <johannes@steudle.eu>
2015-02-14 09:35:05 +0100

    Updated paths and submodule

--- oh-my-custom/hosts/jaybook.local.path.zsh
+++ oh-my-custom/hosts/jaybook.local.path.zsh
@@ -2,13 +2,10 @@ echo 'loading jaybook.path.zsh'
 # local path file
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/usr/texbin
 # android paths
+export PATH=$PATH:/usr/local/Cellar/android-sdk/24.0.2/bin
 export PATH=$PATH:/usr/local/Cellar/android-sdk/24.0.2/tools
 export PATH=$PATH:/usr/local/Cellar/android-sdk/24.0.2/platform-tools
 export PATH=$PATH:/usr/local/Cellar/android-sdk/24.0.2/build-tools/21.1.2
 export PATH=$PATH:/usr/local/Cellar/android-ndk/r10d
 
-export PATH=$PATH:$HOME/QtSDK/Desktop/Qt/474/gcc/bin
-
-export NODE_PATH=/usr/local/lib/node
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages/:$PYTHONPATH
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/Users/johannes/QtSDK/Desktop/Qt/474/gcc/lib/pkgconfig/
