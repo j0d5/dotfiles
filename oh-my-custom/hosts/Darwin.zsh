780b9df89c1650cfd2052ce45bcc72368437fa9b
Johannes Steudle <johannes@steudle.eu>
2015-06-01 09:10:40 +0200

    Add new alias for darwin systems for updating brew

--- oh-my-custom/hosts/Darwin.zsh
+++ oh-my-custom/hosts/Darwin.zsh
@@ -4,5 +4,6 @@ echo "i'm on a darwin system!"
 alias top='top -o cpu -n 30'
 alias downtime='pmset -g log | grep -e " Sleep  " -e " Wake  "'
 alias pf="open -a \"Path Finder.app\""
+alias bu="brew update && brew upgrade --all && brew cleanup"
 
 plugins+=(osx brew)
