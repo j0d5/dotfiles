88bb5b0b667e2c1407e0fcdbe4664627dbc16929
Johannes Steudle <johannes@steudle.eu>
2015-12-14 21:13:12 +0100

    Move to new install configuration

---
+++ .zprofile
@@ -0,0 +1,10 @@
+
+# Setting PATH for Python 2.7
+# The orginal version is saved in .zprofile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
+export PATH
+
+# Setting PATH for Python 3.4
+# The orginal version is saved in .zprofile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
+export PATH
---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+echo "linking script for dotfiles"
+
+# constants
+readonly NAME=".dotfiles"
+readonly DOTDIR="$HOME/$NAME"
+readonly BAK="dotfiles_backup"
+readonly CFGFILES=("zshrc" "vimrc" "gvimrc" "gitignore"\
+                   "gitattributes" "hgrc" "hgignore"\
+                   "liquidpromptrc" "slate" "dir_colors")
+
+# default values
+FIRSTNAME="Johannes"
+LASTNAME="Steudle"
+EMAIL="jaycloudy@icloud.com"
+GITHUBUSER="johannes82"
+
+source $HOME/$NAME/lib.sh
+
+
+if [[ -z $1 ]]; then
+  FIRSTNAME=$1
+fi
+if [[ -z $2 ]]; then
+  LASTNAME=$2
+fi
+if [[ -z $3 ]]; then
+  EMAIL=$3
+fi
+if [[ -z $4 ]]; then
+  GITHUBUSER=$4
+fi
+
+install_dotfiles() {
+  action 'check for existing dotfiles'
+  if ! [ -d $DOTDIR ]; then
+    running "$DOTDIR directory does not exist, cloning..."
+    # mkdir $HOME/$NAME
+    git clone https://github.com/johannes82/.dotfiles.git
+    ok
+  else
+    running "$DOTDIR directory exists" ok
+  fi
+  ok
+}
+
+action "Check on which system we are"
+if [ "Darwin" == $(uname) ]; then
+  running "we're on a mac" ok
+  install_dotfiles
+  require_brew
+else
+  fail "this system is currently not supported"
+fi
+
+check_zsh
+
+action "Linking the dotfiles"
+for file in ${CFGFILES[@]}
+do
+  #action "Create link for $file"
+  link_dotfiles $file
+done
+
+$HOME/$NAME/osx.sh
+
+running 'Everything installed!' ok
+
+
+# echo "linking sublime files..."
+# if [[ -d ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ]]; then
+#   cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+#   ln -is ~/.dotfiles/sublime_user/Preferences.sublime-settings
+#   ln -is ~/.dotfiles/sublime_user/CheckBounce.sublime-settings
+#   ln -is ~/.dotfiles/sublime_user/Default\ \(OSX\).sublime-settings
+#   ln -is ~/.dotfiles/sublime_user/LaTeX.sublime-settings
+#   ln -is ~/.dotfiles/sublime_user/Package\ Control.sublime-settings
+#   ln -is ~/.dotfiles/sublime_user/Side\ Bar.sublime-settings
+# else
+#   echo "ERROR: \
+#      ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User \
+#      does not exist."
+# fi
