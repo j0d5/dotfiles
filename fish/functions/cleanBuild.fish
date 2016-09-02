3f2b9817c238f96716f3126813011a56e4484431
Johannes Steudle <jaycloudy@icloud.com>
2016-09-02 11:40:17 +0200

    Fix gitconfig and gitignore

--- fish/functions/cleanBuild.fish
+++ fish/functions/cleanBuild.fish
@@ -1,4 +1,4 @@
 function cleanBuild --description 'Clean directory from gradle build artifacts'
-	find . -maxdepth 3 -type d -name build -or -name obj -exec rm -r '{}' +
-    find . -type f -name \*.so -exec rm '{}' +
+    find . -maxdepth 3 -type d \( -name "build" -or -name "obj" \) -exec rm -r '{}' +
+    find . -type f -name \*.so # -exec rm '{}' +
 end
