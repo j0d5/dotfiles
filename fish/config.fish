459f44fc8620a6a2d2485f044a833fc0a8e42549
tweetjay <tweetjay2@gmail.com>
2018-02-13 09:23:47 +0100

    Add rvm as ruby installation management

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,4 @@
 # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
 # to create a custom plugin instead.
+rvm default
