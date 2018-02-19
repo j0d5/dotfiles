fafe088a86148df0c3e598e7a258c36e74050982
tweetjay <tweetjay2@gmail.com>
2018-02-19 08:39:36 +0100

    Fix lint warnings for checkForDebugBuild

--- bin/checkForDebugBuild.sh
+++ bin/checkForDebugBuild.sh
@@ -17,8 +17,8 @@ checkForDebugBuild () {
 
 __check_architectures () {
   __print "Extracting architectures..." "info"
-  if [ $(which lipo) > /dev/null ]; then
-    lipo -i $1
+  if [ "$(command -v lipo)" ]; then
+    lipo -i "$1"
   fi
   __print
 }
@@ -26,7 +26,7 @@ __check_architectures () {
 __check_for_assertion () {
   __print "Check for assertions..." "info"
 
-  if [[ $(nm $1 | grep NSAssertionHandler) ]]; then
+  if nm "$1" | grep -q "NSAssertionHandler"; then
     __print "Found assertion symbols!" "error"
   else
     __print "No assertion symbols found!"
@@ -40,8 +40,8 @@ __check_debug_symbols () {
   secondFile="/var/tmp/allSymbols.txt"
   __print
 
-  nm $1 > $firstFile
-  nm -a $1 > $secondFile
+  nm "$1" > $firstFile
+  nm -a "$1" > $secondFile
 
   __print "Check if there is a difference..." "info"
   result=$(diff -q $firstFile $secondFile)
@@ -60,9 +60,9 @@ __check_debug_symbols () {
 }
 
 # "Checks if unwanted profiling data are in the library"
-function __check_profiling_data () {
+__check_profiling_data () {
   __print "Check profiling info..." "info"
-  if otool -l -arch all $1 | grep __llvm_; then
+  if otool -l -arch all "$1" | grep __llvm_; then
     __print "Contains profiling data!" "error"
   else
     __print "No profiling info found!"
@@ -71,38 +71,42 @@ __print
 }
 
 # "Checks if the library is encrypted"
-function __check_encryption () {
+__check_encryption () {
   __print "Check encryption info..." "info"
-  if otool -l $1 | grep -A 4 LC_ENCRYPTION_INFO; then
+  if otool -l "$1" | grep -A 4 LC_ENCRYPTION_INFO; then
     __print "Encryption info found!"
   else
     __print "No encryption info found!"
   fi
-__print
+  __print
 }
 
 # fonts color
-FRED="31m"
-FGREEN="32m"
-FYELLOW="33m"
-FBLUE="34m"
-FPURPLE="35m"
-FWHITE="0m"
+RED="31m"
+GREEN="32m"
+YELLOW="33m"
+BLUE="34m"
+WHITE="0m"
 
 __print () {
-  if [ "$2" == "info" ] ; then
-      COLOR=$FBLUE
-  elif [ "$2" == "success" ] ; then
-      COLOR=$FGREEN
-  elif [ "$2" == "warning" ] ; then
-      COLOR=$FYELLOW
-  elif [ "$2" == "error" ] ; then
-      COLOR=$FRED
+  if [ "$2" = "info" ] ; then
+      COLOR=$BLUE
+  elif [ "$2" = "success" ] ; then
+      COLOR=$GREEN
+  elif [ "$2" = "warning" ] ; then
+      COLOR=$YELLOW
+  elif [ "$2" = "error" ] ; then
+      COLOR=$RED
   else
-      COLOR=$FWHITE
+      COLOR=$WHITE
   fi
 
-  echo "\033[0;$COLOR" "▸ $1" "\033[0m"
+  echo "\\033[0;$COLOR" "▸ $1" "\\033[0m"
 }
 
-checkForDebugBuild $1
+if [ -e "$1" ]; then
+  checkForDebugBuild "$1"
+else
+  __print "File $1 not found!" "error"
+fi
+
