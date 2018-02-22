d70ae57f620376fbaedb8eadd61b7afa0a32b2ec
tweetjay <tweetjay2@gmail.com>
2018-02-22 20:58:22 +0100

    Add the rest of the tools and remove Makefiles

---
+++ osx/check_brew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if ! command -v brew; then
+	curl -fsSo "$TMPDIR"/brew-install.rb https://raw.githubusercontent.com/Homebrew/install/master/install
+	/usr/bin/ruby "$TMPDIR"/brew-install.rb
+fi
+
