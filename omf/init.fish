2c7219205fba7353e780930602f1b1288c4bf220
tweetjay <tweetjay2@gmail.com>
2018-05-18 15:57:11 +0200

    Fix vim dotbot file and omf init

--- omf/init.fish
+++ omf/init.fish
@@ -23,14 +23,11 @@ if test $TERM != "screen-256color"
 end
 
 set -gx  LC_ALL en_US.UTF-8
-if test ~/.bin
-  set -gx PATH $PATH $HOME/.bin
-end
 
 set -gx NPM_PACKAGES "$HOME/.npm-packages"
 
 set -gx PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin /usr/local/lib /usr/lib
-set -gx PATH $PATH $HOME/.dotfiles/bin
+set -gx PATH $PATH (pwd)/bin
 set -gx PATH $PATH /usr/local/MacGPG2/bin /Library/TeX/texbin
 set -gx PATH $PATH /Applications/Wireshark.app/Contents/MacOS
 set -gx PATH $PATH $HOME/.gem/ruby/2.4.0/bin $HOME/.rvm/bin
