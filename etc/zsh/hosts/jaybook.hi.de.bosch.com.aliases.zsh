632d72954683d61acb7b35492894ce029d3ec780
tweetjay <tweetjay2@gmail.com>
2018-06-09 12:29:55 +0200

    Fix broken path entry
    Add lldb config
    Fix missing bin link
    Update Brewfile

--- etc/zsh/hosts/jaybook.hi.de.bosch.com.aliases.zsh
+++ etc/zsh/hosts/jaybook.hi.de.bosch.com.aliases.zsh
@@ -13,24 +13,8 @@ if [[ -n $DEBUG_ZSH ]]; then
 fi
 
 # general
-
-# rsync
-alias syncDocs='rsync -vrhzu --exclude-from ~/Documents/exclude.rsync ~/Documents/ /Volumes/jst2hi\$/Eigene\ Dateien/'
-
-# android aliases
-alias andup='android update project -p .'
-alias andut='android update test-project -m ../ -p .'
-alias package="aapt dump badging $* | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g"
-alias activity="aapt dump badging $* | grep Activity | awk '{print $2}' | sed s/name=//g | sed s/\'//g"
-
-# adb aliases
-alias adbr='adb kill-server && adb start-server'
-alias adbk='adb -e emu kill'
-
-# gradle
-alias cleanBuild='find . -maxdepth 3 -type d -name build -or -name obj -exec rm -r {} +'
-alias buildSdk='gradle :Source:Service:clean :Source:ServerSDK:clean :Source:Service:build :Source:ServerSDK:build'
-alias buildMini='gradle :Testing:AudioManagementTest:clean :Testing:AudioManagementTest:build'
+alias cleanBaks='find . -type f -name \*.orig -exec rmtrash "{}" +'
+alias cleanBuild='find . -type d -name build -exec rmtrash -r "{}" +'
 
 # ssh aliases
 alias sshRefSys='ssh -Y johannes-ref'
