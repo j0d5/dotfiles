fb929e4dfa4afff2879557abce2034c697079954
tweetjay <tweetjay2@icloud.com>
2019-04-30 17:24:49 +0200

    Improve tag script

--- bin/git-delete-tags.sh
+++ bin/git-delete-tags.sh
@@ -8,24 +8,52 @@
 #
 # }}}
 
+# Fonts color
+RED="31m"
+GREEN="32m"
+YELLOW="33m"
+BLUE="34m"
+WHITE="0m"
+
+__print () {
+    if [ "$2" = "info" ] ; then
+        COLOR=$BLUE
+    elif [ "$2" = "success" ] ; then
+        COLOR=$GREEN
+    elif [ "$2" = "warning" ] ; then
+        COLOR=$YELLOW
+    elif [ "$2" = "error" ] ; then
+        COLOR=$RED
+    else
+        COLOR=$WHITE
+    fi
+
+    echo "\\033[0;$COLOR" "▸ $1" "\\033[0m"
+}
+
 # Check if we're in valid git repository
 if ! [ -d ".git" ]; then
-  echo "No git repository!"
+  __print "No git repository!" "error"
   exit 1
 fi
 
+__print "Fetch latest tags ..."
+git fetch --tags
+
+# Enter all valid patterns for tags, e.g. : 'release' will match all 'release/0.0.0.0' (major, minor, patch, optional build)
 VALID_TAGS="release|ff|dr"
+# Get all invalid tags with inverse regex
 INVALID_TAGS=$(git tag | grep -vE "($VALID_TAGS)\/[0-9].[0-9].[0-9](.[0-9]+)?")
 
 # Check if there are any invalid tags
 if [ -z "$INVALID_TAGS" ]; then
-  echo "No invalid tags found."
+  __print "No invalid tags found." "success"
   exit 0
 fi
 
 # Check if we really want to delete those tags
-echo "$INVALID_TAGS"
-echo "Delete the following tags? [Y/N]"
+__print "$INVALID_TAGS"
+__print "Delete the tags? [Y/N]" "warning"
 read -r ANSWER
 case "$ANSWER" in
   [Yy]|[Yy][Ee][Ss]) echo "Deleting tags ...";;
@@ -41,6 +69,6 @@ for TAG in $INVALID_TAGS; do
 done
 
 # Clean those local tags
-echo "Cleaning local tags ..."
+__print "Cleaning local tags ..."
 git fetch --prune origin "+refs/tags/*:refs/tags/*"
-echo "Done."
+__print "Done."
