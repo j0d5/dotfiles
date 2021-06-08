ee8c0d88abe1875add7621051640441e737cc7fb
tweetjay <tweetjay2@icloud.com>
2021-06-08 14:01:37 +0200

    Update vim config & Powerlevel subrepo

--- zsh/config/global_settings.zsh
+++ zsh/config/global_settings.zsh
@@ -26,6 +26,11 @@ export HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS=i
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white'
 
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
 # Autosuggestion settings
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
