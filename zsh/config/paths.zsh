28c3992029d5a87e205832144670d364bf4680d9
Johannes Steudle <johannes@steudle.eu>
2022-03-28 09:17:42 +0200

    [ZSH] Fix paths

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -7,7 +7,15 @@
 #
 # }}}
 
-export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin:$PATH
+# Put homebrew stuff first
+export PATH=/opt/homebrew/bin:/opt/homebrew/sbin
+# User specific paths
+export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+# System specific paths
+export PATH=$PATH:/bin:/sbin
+# macOS stuff
+export PATH=$PATH:/Library/Apple/usr/bin:/Applications/Wireshark.app/Contents/MacOS
+# Own stuff
 export PATH=$PATH:$HOME/.bin
 
 export NVM_DIR="$HOME/.nvm"
