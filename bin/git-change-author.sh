78f71a971fdfd3e2e352a3e24881f66ed20c452d
Johannes Steudle <johannes@steudle.eu>
2022-10-19 12:38:42 +0200

    fix: Remove hardcoded email

--- bin/git-change-author.sh
+++ bin/git-change-author.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Missing parameter, usage git-change-author.sh <old email> <new name> <new email>"
+    exit 1
+fi;
+
+echo "Changing author from: $1 to name: $2 and email: $3"
+
 git filter-branch --env-filter '
-OLD_EMAIL="johannes.steudle@bosch-softtec.com"
-CORRECT_NAME="tweetjay"
-CORRECT_EMAIL="tweetjay2@gmail.com"
+OLD_EMAIL=$1
+CORRECT_NAME=$2
+CORRECT_EMAIL=$3
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
